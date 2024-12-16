fcb781c97d9a0583fc86d2a2f94755524de8ff56
Mañana <Carlos Jimenez>
2024-12-16 13:38:55 +0100

    Primer Commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground 
+      style={styles.fondo}
+      source = {require('./assets/1-583fbaed.png')}>
+    
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  fondo:{
+    marginTop:300
+  }
 });
